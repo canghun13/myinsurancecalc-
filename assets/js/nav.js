4f7d0c8330948ee6d0d28ec4c633406bdac217d6
canghun13 <41933428+canghun13@users.noreply.github.com>
2026-05-05 10:41:40 +0900

    Add files via upload

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,3 +1,15 @@
+// Google Analytics
+(function() {
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-TB2KNRH23W';
+  document.head.appendChild(script);
+})();
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-TB2KNRH23W');
+
 document.addEventListener('DOMContentLoaded', function () {
   const nav = `
   <header class="site-header">
